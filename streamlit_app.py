21351deb0c535d9da8fc6fb48fe3a795d39f2dfb
Kaden McKeen <mckeen.kaden.school@hotmail.com>
2022-02-08 22:17:33 -0500

    Update navbar.

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,6 +8,40 @@ st.set_page_config(page_title="Kaden McKeen")#, layout="wide")
 with open("style.css") as f:
     st.markdown('<style>{}</style>'.format(f.read()), unsafe_allow_html=True)
 
+#####################
+# Navigation
+st.markdown('''
+  <head>
+    <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" />
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+    <script type="text/javascript" src="Scripts/jquery-2.1.1.min.js"></script>
+    <script type="text/javascript" src="Scripts/bootstrap.min.js"></script>
+  </head>
+''', unsafe_allow_html=True)
+
+st.markdown("""
+<nav class="navbar fixed-top navbar-expand navbar-light bg-light" style="background-color: #16A2CB;">
+  <a class="navbar-brand" href="https://www.linkedin.com/in/kadenmckeen/" target="_blank">Kaden McKeen</a>
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="https://share.streamlit.io/kadenmc/streamlit-site/main">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#projects">Projects</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#publications">Publications</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="https://kadenmc.github.io/cv/cv.html" target="_blank">CV</a>
+      </li>
+    </ul>
+</nav>
+""", unsafe_allow_html=True)
+
+
 # Header 
 st.write('''# Kaden McKeen''')
 
@@ -41,35 +75,6 @@ st.info('''
 
 st.markdown('<img src="https://kadenmc.github.io/assets/Icon.png" style="max-height: 300px; border-radius:50px; margin-left: 10%;" />', \
   unsafe_allow_html=True)
-
-#####################
-# Navigation
-st.markdown('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">', unsafe_allow_html=True)
-st.markdown("""
-<nav class="navbar fixed-top navbar-expand-lg navbar-dark" style="background-color: #16A2CB;">
-  <a class="navbar-brand" href="https://www.linkedin.com/in/kadenmckeen/" target="_blank">Kaden McKeen</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarNav">
-    <ul class="navbar-nav">
-      <li class="nav-item active">
-        <a class="nav-link" href="https://share.streamlit.io/kadenmc/streamlit-site/main">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#projects">Projects</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#publications">Publications</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="https://kadenmc.github.io/cv/cv.html" target="_blank">CV</a>
-      </li>
-    </ul>
-  </div>
-</nav>
-""", unsafe_allow_html=True)
-
 
 
 #####################
