7d1627c5eebc95405948b2bd45120c1593dac982
Kaden McKeen <mckeen.kaden.school@hotmail.com>
2022-02-09 00:19:44 -0500

    Fix CV button.

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,4 +30,42 @@ elif menu_id == "Projects":
 elif menu_id == "Publications":
   publications.main()
 elif menu_id == "CV":
-  webbrowser.open_new_tab("https://kadenmc.github.io/cv/cv.html")
+  st.markdown('''
+    <style>
+      button {
+        position: relative;
+        margin: 0;
+        padding: 5px 12px;
+        height: 60px;
+        width: 150px;
+        outline: none;
+        text-decoration: none;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        cursor: pointer;
+        text-transform: uppercase;
+        background-color: #ffffff;
+        border: 1px solid rgba(22, 76, 167, 0.6);
+        border-radius: 10px;
+        color: #1d89ff;
+        font-weight: 400;
+        font-size: 20px;
+        font-family: inherit;
+        z-index: 0;
+        overflow: hidden;
+        transition: all 0.3s cubic-bezier(0.02, 0.01, 0.47, 1);
+        span {
+          color: #164ca7;
+          font-size: 12px;
+          font-weight: 500;
+          letter-spacing: 0.7px;
+      }
+    </style>
+
+    <a href="https://kadenmc.github.io/cv/cv.html" target="_blank" style="text-decoration: none;">
+      <button class="btn-slide-line center">
+        <span>Curriculum Vitae</span>
+      </button>
+    </a>
+''', unsafe_allow_html=True)
